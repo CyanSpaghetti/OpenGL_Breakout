46e31436e55ac4065f797746a18845b88f49e48c
CyanSpaghetti <tp.arash.m@gmail.com>
2023-06-05 08:46:29 +0500

    Added Boxes on x axis

--- OpenGL_Breakout.cpp
+++ OpenGL_Breakout.cpp
@@ -12,14 +12,15 @@ const int width = 640, height = 480;
 const char* title = "Breakout";
 
 //Player
-int px = 320, py = 300, sizeX=24, sizeY=8, pspeed=4;
-
-//Boxes
+int px = 320, py = 400, sizeX=24, sizeY=8, pspeed=16;
 
 //Ball
 float bx, by; int bsize;
-float bspeed = 0.2f, bxvel, byvel;
+float bspeed = 0.1f, bxvel, byvel;
 
+//Boxes
+int _bsizeX = 24, _bsizeY = 12, bq = 13, tb = 0;
+int _bx[18] = {}, _by[18] = {};
 
 void DrawPlayer() // Draw Player rect using triangles
 {
@@ -73,9 +74,62 @@ void BallMove()
 	}
 }
 
+void InitBoxes()
+{
+	for (int i = 1; i < bq; i++)
+	{
+		_bx[i-1] = width - _bsizeX*i*2;
+		_by[i-1] = 100;
+	}
+}
+
+void DrawBoxes()
+{
+	for (int i = 0; i < bq; i++)
+	{
+		if (_bx[i] == NULL) continue;
+		glColor3f(0, 1, 1);
+
+		glBegin(GL_TRIANGLES);
+		glVertex2i(_bx[i] - _bsizeX+1, _by[i] - _bsizeY+1);
+		glVertex2i(_bx[i] + _bsizeX-1, _by[i] - _bsizeY+1);
+		glVertex2i(_bx[i] - _bsizeX+1, _by[i] + _bsizeY-1);
+
+		glVertex2i(_bx[i] + _bsizeX-1, _by[i] + _bsizeY-1);
+		glVertex2i(_bx[i] + _bsizeX-1, _by[i] - _bsizeY+1);
+		glVertex2i(_bx[i] - _bsizeX+1, _by[i] + _bsizeY-1);
+		glEnd();
+	}
+}
+
+void RemoveBoxes()
+{
+	for (int i = 0; i < bq; i++)
+	{
+		if (_bx[i] == NULL) continue;
+		if (bx + bxvel < _bx[i] + _bsizeX && bx + bxvel > _bx[i] - _bsizeX && by + byvel < _by[i] + _bsizeY && by + byvel > _by[i] - _bsizeY)
+		{
+			_bx[i] = NULL; _by[i] = NULL;
+			tb++;
+			ChangeBallVel(1, -1);
+		}
+	}
+}
+
+void WinCheck()
+{
+	if (tb == bq-1)
+	{
+		std::cout << " You Won! " << std::endl;
+		exit(0);
+	}
+}
+
 void physics()
 {
+	WinCheck();
 	BallMove();
+	RemoveBoxes();
 	glutPostRedisplay(); // since i don't understand the timer or idle functions of glut, i use recursion!!
 }
 
@@ -89,6 +143,7 @@ void render(void)
 	//Render Player
 	DrawPlayer();
 	// Render all Boxes
+	DrawBoxes();
 	// -- End of Drawing -- //	
 	physics();
 	glutSwapBuffers();
@@ -114,6 +169,7 @@ void Init()
 	px = 300; py = 400; // fixes y position of player
 	bx = width / 2; by = height / 2; // sets ball to center
 	bxvel = bspeed; byvel = bspeed; // changes the velocity of the ball to move in a NW direction
+	InitBoxes(); // create array and stuff
 	glutPostRedisplay(); // render game
 }
 
